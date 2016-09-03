8e0ee269f0b162b38fe49e123a34a5b9345c9190
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-09-03 14:08:17 -0500

    got everything fully dynamic

--- wysiwyg.js
+++ wysiwyg.js
@@ -1,35 +1,28 @@
-
+var famousPeople = [];
+var userInputBtn = document.getElementById("userInput");
 var userInputText = document.getElementById("inputText");
 var outputEl = document.getElementById("outputEl");
 var clickBio = "";
-
 var finalOutput = "";
 var outputDOM = "";
 
-var famousPeople = [{
-		title: "Samurai",
-  		name: "Tomoe Gozen",
-  		bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  		image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
-  		lifespan: {
-    		birth: 1747,
-    		death: 1797
+function famousPpl() {
+famousPeople.push({
+		title: document.getElementById('title').value,
+		name: document.getElementById('name').value,
+		bio: document.getElementById('bio').value,
+		image: document.getElementById('image').value,
+		lifespan: {
+			birth: document.getElementById('birth').value,
+			death: document.getElementById('death').value,
 		}
-		},
-{
-		title: "Samurai",
-  		name: "Tomoe Gozen",
-  		bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-  		image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
-  		lifespan: {
-    		birth: 1747,
-    		death: 1797
-		}
-		}];
-	
-
+});
 getFamoustPpl(famousPeople);
+}	
+userInputBtn.addEventListener('click', famousPpl);
+
 function getFamoustPpl(obj) {
+	outputEl.innerHTML = "";
 	for (var counter = 0; counter < obj.length; counter++) {
 		outputEl.innerHTML += `<person class="person_container" id="person--${counter}"><header> ${obj[counter].title}: ${obj[counter].name}</header> <section><p class="bio" id="bio--${counter}">${obj[counter].bio}</p><img src="${obj[counter].image}"></section> <footer>${obj[counter].lifespan.birth} - ${obj[counter].lifespan.death}</footer></person>`; 
 			//outputDOM += textInput;
@@ -59,94 +52,3 @@ userInputText.addEventListener("keyup", function(e) {
 	}
 
 })
-
-	//outputPersonDiv.innerHTML = outputDOM;
-	//console.log(outputPersonDiv.childNodes);
-	//addTextArea();
-
-	//getValue();	
-
-// function addTextArea() {
-	
-// 	for (var i = 0; i < personDiv.length; i++){
-// 		var inputUser = document.createElement("input");
-// 		inputUser.id = "bioInput";
-// 		inputUser.type = "text"
-// 		personDiv[i].appendChild(inputUser);
-// 	}
-// 	getValue();
-// }
-// //var bioArea = document.getElementById("bioInput");
-// function getValue() {
-// 	var bioList = document.getElementsByClassName("bio");
-// 	for (var i = 0; i < bioList.length; i++){
-// 		bioInput[i].addEventListener("change", function() {
-			
-// 			//console.log(event.target.value);
-// 			console.log(bioList.click);
-// 		});
-// 	}	
-// }
-
-
-// var counter = 0;
-// function parseList(obj) { 
-// 	for (; counter < 5; counter++){
-// 		outputDOM += "<person class='person' id='person--${counter}'>" + obj + "</person>"
-// 	}
-// 	outputPersonDiv.innerHTML += outputDOM;
-// }
-// //userInputBtn.addEventListener('click', famousPpl);
-// // var counter = 0;
-// // function parseList(obj) {
-// // 	for (var i = 0; i < obj.length; i++) {
-
-// // 		outputDOM += "<person class='person' id='person--${i}'> <header>" + obj[i].title + ": " + obj[i].name + "</header> <section>" + "<p>" + obj[i].bio + "</p>"+ obj[i].image + "</section> <footer>" + obj[i].lifespan.birth + " - " + obj[i].lifespan.death + "</footer> <input type='text'></type></person>"; 
-// // 		//outputDOM += textInput;
-
-// // 	}
-// // 	outputPersonDiv.innerHTML += outputDOM;
-// // 	console.log(personDiv);
-// // }
-
-// for (var i = 0; i < personDiv.length; i++){
-// 	personDiv[i].addEventListener("click", function(event) {
-// 		event.target.classList.add("dotted");
-// 	})
-// }
-// function clickingDiv(event) {
-// 	personDiv.addEventListener("click", function(event) {
-// 		console.log(event);
-// 		console.log(event.target);
-// 		console.log(event.currentTarget);
-
-// 	});
-//  }
-
-
-
-// var counter = 0;
-// var outputEl = document.getElementById("outputEl");
-// for (; counter < 5; counter++) {
-//   // Give each person element a unique identifier
-//   outputEl.innerHTML += `<div class="person__container" id="person--${counter}"></div>`;
-// }
-
-// // Now containerEl will have elements in it
-// var containerEl = document.getElementsByClassName("person__container");
-
-// // Event listeners are created
-// for (var i = 0; i < containerEl.length; i++) {
-//   containerEl[i].addEventListener("click", function (event) {
-//     // Logic to execute when the element is clicked
-//   });
-// };
-// var arr = [];
-// var len = oFullResponse.results.length;
-// for (var i = 0; i < len; i++) {
-//     arr.push({
-//         key: oFullResponse.results[i].label,
-//         sortable: true,
-//         resizeable: true
-//     });
-// }
